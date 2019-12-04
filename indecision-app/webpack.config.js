a706447490ae7146d8971155ca1f8d7131a6ceaf
Muthu Venkatachalam <muthukarupasamy.venkatachalam@resideo.com>
2019-12-04 05:53:49 +0530

    added web dev server packages, plugin for class prop

--- indecision-app/webpack.config.js
+++ indecision-app/webpack.config.js
@@ -12,5 +12,9 @@ module.exports = {
       exclude:/node_modules/,
       loader:'babel-loader'
     }]
+  },
+  devtool:'cheap-module-eval-source-map',
+  devServer:{
+    contentBase:path.join(__dirname,'public')
   }
 }
