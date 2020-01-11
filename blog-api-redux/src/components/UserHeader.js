dff6138b2a9606a27c4e12c0362aaa136f909544
Muthu Venkatachalam <muthukarupasamy.venkatachalam@resideo.com>
2020-01-11 16:36:13 +0530

    init commit

---
+++ blog-api-redux/src/components/UserHeader.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {fetchPosts} from '../actions'
+
+class UserHeader extends Component{
+  render(){
+    return <div>User Header</div>
+  }
+}
+
+export default UserHeader
